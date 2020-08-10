a0155d60ec0adcb92945b36477fbdccba222f77d
robinxw <robin.xw@hotmail.com>
2020-08-10 16:37:43 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# For newly installed Linux system save time from setting up
+## Following software will be installed:
+Name|Link
+---|---
+terminator|[Detail](https://launchpad.net/terminator)
+VS Code|[Detail](https://code.visualstudio.com/)
+Oh my zsh|[Detail](https://ohmyz.sh/)
+## It also does: 
+- Set no password for using sudo for all users in sudo group
+- Change zsh theme to `agnoster`
+- Install newest python3 and pip3
+- alias python3 and pip3 to python and pip in `zshrc`
---
+++ ubuntuSetup.sh
@@ -0,0 +1,16 @@
+sudo grep -qxF '%sudo   ALL=(ALL:ALL) NOPASSWD:ALL' /etc/sudoers || echo "%sudo   ALL=(ALL:ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
+sudo apt update -y
+sudo apt upgrade -y
+python3 -V
+sudo apt install -y python3-pip
+sudo apt install -y git vim curl wget
+sudo apt install -y terminator zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo apt install -y fonts-powerline xclip
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
+echo "alias python=python3" >> ~/.zshrc
+echo "alias pip=pip3" >> ~/.zshrc
+source ~/.zshrc
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
