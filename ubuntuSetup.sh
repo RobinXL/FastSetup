e8a15c3e13827ead02e42369dced5c007d850f14
Ubuntu <ubuntu@ip-172-31-77-158.ec2.internal>
2020-08-20 02:07:25 +0000

    handle manually change zsh to default

--- ubuntuSetup.sh
+++ ubuntuSetup.sh
@@ -14,3 +14,4 @@ wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt install code
 source ~/.zshrc
+sudo chsh -s $(which zsh) $(whoami)
